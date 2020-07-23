900b7b3bdea285b3a933d7e46108b525cb826aae
freelancerifaz <mrifaz369@gmail.com>
2020-07-23 17:08:43 +0600

    added case cart decrease section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // add iPhone in cart
 
-const iphoneBtn = document.getElementById("iphonePlus")
-iphoneBtn.addEventListener("click" , function(){
+const iphonePlusBtn = document.getElementById("iphonePlus")
+iphonePlusBtn.addEventListener("click" , function(){
     const iphoneNumber = document.getElementById("iphoneNumber").value;
     const currentIphoneNum = parseFloat(iphoneNumber);
     document.getElementById("iphoneNumber").value = currentIphoneNum + 1;
@@ -22,8 +22,8 @@ iphoneBtn.addEventListener("click" , function(){
 
 // decrease iphone in cart
 
-const caseBtn = document.getElementById("iphoneMinus");
-caseBtn.addEventListener("click" , function(){
+const iphoneMinusBtn = document.getElementById("iphoneMinus");
+iphoneMinusBtn.addEventListener("click" , function(){
     const iphoneNumber = document.getElementById("iphoneNumber").value;
     const currentIphoneNum = parseFloat(iphoneNumber);
     document.getElementById("iphoneNumber").value = currentIphoneNum - 1;
@@ -39,4 +39,46 @@ caseBtn.addEventListener("click" , function(){
     const total = document.getElementById("total").innerText;
     const totalNum = parseFloat(total);
     document.getElementById("total").innerText = totalNum - 1280;
+})
+
+// add iPhone case in cart
+
+const casePlusBtn = document.getElementById("casePlus");
+casePlusBtn.addEventListener("click", function(){
+    const caseNumber = document.getElementById("caseNumber").value;
+    const currentCaseNum = parseFloat(caseNumber);
+    document.getElementById("caseNumber").value = currentCaseNum + 1;
+    const currentCasePrice = document.getElementById("currentCasePrice").innerText;
+    const casePriceNum = parseFloat(currentCasePrice);
+    document.getElementById("currentCasePrice").innerText = casePriceNum + 59;
+    const subTotal = document.getElementById("subTotal").innerText;
+    const subTotalNum = parseFloat(subTotal);
+    document.getElementById("subTotal").innerText = subTotalNum + 59;
+    const tax = document.getElementById("tax").innerText;
+    const taxNum = parseFloat(tax);
+    document.getElementById("tax").innerText = taxNum + 2;
+    const total = document.getElementById("total").innerText;
+    const totalNum = parseFloat(total);
+    document.getElementById("total").innerText = totalNum + 59;
+})
+
+//decrease iPhone case in cart
+
+const caseMinusBtn = document.getElementById("caseMinus");
+caseMinusBtn.addEventListener("click" , function(){
+    const caseNumber = document.getElementById("caseNumber").value;
+    const currentCaseNum = parseFloat(caseNumber);
+    document.getElementById("caseNumber").value = currentCaseNum - 1;
+    const currentCasePrice = document.getElementById("currentCasePrice").innerText;
+    const caseNum = parseFloat(currentCasePrice);
+    document.getElementById("currentCasePrice").innerText = caseNum - 59;
+    const subTotal = document.getElementById("subTotal").innerText;
+    const subTotalNum = parseFloat(subTotal);
+    document.getElementById("subTotal").innerText = subTotalNum - 59;
+    const tax = document.getElementById("tax").innerText;
+    const taxNum = parseFloat(tax);
+    document.getElementById("tax").innerText = taxNum - 2;
+    const total = document.getElementById("total").innerText;
+    const totalNum = parseFloat(total);
+    document.getElementById("total").innerText = totalNum - 59;
 })
